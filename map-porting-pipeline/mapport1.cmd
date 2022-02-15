def18e9d9c0f61728f29941f16af5912b024b7e4
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2022-02-15 11:16:42 -0500

    Create mapport1.cmd

---
+++ map-porting-pipeline/mapport1.cmd
@@ -0,0 +1,60 @@
+# Port armory
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\armory.map"
+porttag *.scnr
+exit
+# Port chillout
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\chillout.map"
+porttag *.scnr
+exit
+# Port construct
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\construct.map"
+porttag *.scnr
+exit
+# Port Descent
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\descent.map"
+porttag *.scnr
+exit
+# Port Longshore
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\docks.map"
+porttag *.scnr
+exit
+# Port fortress
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\fortress.map"
+porttag *.scnr
+exit
+# Port ghosttown
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\ghosttown.map"
+porttag *.scnr
+exit
+# Port lockout
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\lockout.map"
+porttag *.scnr
+exit
+# Port midship
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\midship.map"
+porttag *.scnr
+exit
+# Port salvation
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\salvation.map"
+porttag *.scnr
+exit
+# Port sandbox
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\sandbox.map"
+porttag *.scnr
+exit
+# Port sidewinder
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\sidewinder.map"
+porttag *.scnr
+exit
+# Port snowbound
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\snowbound.map"
+porttag *.scnr
+exit
+# Port spacecamp
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\spacecamp.map"
+porttag *.scnr
+exit
+# Port warehouse
+opencachefile "C:\Users\Documents\ElDewritoServer\ElDewrito\ElDewritoEnhanced\ElDewrito 0.6\Halo Online\dlcmaps\warehouse.map"
+porttag *.scnr
+exit
