68bae3abcae793ac4f0b621f15e583cc145f5d9e
2174497765446E744D <96374300+2174497765446E744D@users.noreply.github.com>
2022-02-19 18:50:51 +1100

    Create ED Enhanced Map Porting Script

---
+++ ElDewrito Enhanced/ED_Enhanced.bat
@@ -0,0 +1,9 @@
+@ echo off
+
+ECHO PORTING MAPS....
+
+cd TT1
+
+TYPE ED_Enhanced_Script_1.cmd | TagTool.exe ../EDE/tags.dat
+
+ECHO PORT COMPLETE! PLEASE CHECK FOR ERRORS.
---
+++ ElDewrito Enhanced/TT1/ED_Enhanced_Script_1.cmd
@@ -0,0 +1,74 @@
+# Port Construct
+opencachefile "../H3Maps/construct.map"
+porttag !ms30 *.scnr
+exit
+# Port Isolation
+opencachefile "../H3Maps/isolation.map"
+porttag !ms30 *.scnr
+exit
+# Port Epitaph
+opencachefile "../H3Maps/salvation.map"
+porttag !ms30 *.scnr
+exit
+# Port Snowbound
+opencachefile "../H3Maps/snowbound.map"
+porttag !ms30 *.scnr
+exit
+updatemapfilesalt "../H3Maps/info"
+# Port Rat's Nest
+opencachefile "../H3MythicMaps/armory.map"
+porttag !ms30 *.scnr
+exit
+# Port Cold Storage
+opencachefile "../H3MythicMaps/chillout.map"
+porttag !ms30 *.scnr
+exit
+# Port Assembly
+opencachefile "../H3MythicMaps/descent.map"
+porttag !ms30 *.scnr
+exit
+# Port Longshore
+opencachefile "../H3MythicMaps/docks.map"
+porttag !ms30 *.scnr
+exit
+# Port Citadel
+opencachefile "../H3MythicMaps/fortress.map"
+porttag !ms30 *.scnr
+exit
+# Port Ghost Town
+opencachefile "../H3MythicMaps/ghosttown.map"
+porttag !ms30 *.scnr
+exit
+# Port Blackout
+opencachefile "../H3MythicMaps/lockout.map"
+porttag !ms30 *.scnr
+exit
+# Port Heretic
+opencachefile "../H3MythicMaps/midship.map"
+porttag !ms30 *.scnr
+exit
+# Port Sandbox
+opencachefile "../H3MythicMaps/sandbox.map"
+porttag !ms30 *.scnr
+exit
+# Port Avalanche
+opencachefile "../H3MythicMaps/sidewinder.map"
+porttag !ms30 *.scnr
+exit
+# Port Orbital
+opencachefile "../H3MythicMaps/spacecamp.map"
+porttag !ms30 *.scnr
+exit
+# Port Foundry
+opencachefile "../H3MythicMaps/warehouse.map"
+porttag !ms30 *.scnr
+exit
+updatemapfilesalt "../H3MythicMaps/info"
+opencachefile "../H3MCCMaps/s3d_waterfall.map"
+# Port Waterfall
+porttag !ms30 *.scnr
+exit
+updatemapfilesalt "../H3MCCMaps/info"
+dumplog ED_Enhanced_log.txt
+
+
