905f3298fdc917d3d6b9f91259be976ded1f37d6
2174497765446E744D <96374300+2174497765446E744D@users.noreply.github.com>
2022-02-20 19:27:40 +1100

    Add Custom Map Info Files for Village and Waterfall.

--- ElDewrito Enhanced/TT1/ED_Enhanced_Script_1.cmd
+++ ElDewrito Enhanced/TT1/ED_Enhanced_Script_1.cmd
@@ -1,95 +1,3 @@
-# Port Construct
-opencachefile "../H3Maps/construct.map"
-porttag !ms30 *.scnr
-exit
-# Port Isolation
-opencachefile "../H3Maps/isolation.map"
-porttag !ms30 *.scnr
-exit
-# Port Epitaph
-opencachefile "../H3Maps/salvation.map"
-porttag !ms30 *.scnr
-exit
-# Port Snowbound
-opencachefile "../H3Maps/snowbound.map"
-porttag !ms30 *.scnr
-exit
-# Port Rat's Nest
-opencachefile "../H3MythicMaps/armory.map"
-porttag !ms30 *.scnr
-exit
-# Port Cold Storage
-opencachefile "../H3MythicMaps/chillout.map"
-porttag !ms30 *.scnr
-exit
-# Port Assembly
-opencachefile "../H3MythicMaps/descent.map"
-porttag !ms30 *.scnr
-exit
-# Port Longshore
-opencachefile "../H3MythicMaps/docks.map"
-porttag !ms30 *.scnr
-exit
-# Port Citadel
-opencachefile "../H3MythicMaps/fortress.map"
-porttag !ms30 *.scnr
-exit
-# Port Ghost Town
-opencachefile "../H3MythicMaps/ghosttown.map"
-porttag !ms30 *.scnr
-exit
-# Port Blackout
-opencachefile "../H3MythicMaps/lockout.map"
-porttag !ms30 *.scnr
-exit
-# Port Heretic
-opencachefile "../H3MythicMaps/midship.map"
-porttag !ms30 *.scnr
-exit
-# Port Sandbox
-opencachefile "../H3MythicMaps/sandbox.map"
-porttag !ms30 *.scnr
-exit
-# Port Avalanche
-opencachefile "../H3MythicMaps/sidewinder.map"
-porttag !ms30 *.scnr
-exit
-# Port Orbital
-opencachefile "../H3MythicMaps/spacecamp.map"
-porttag !ms30 *.scnr
-exit
-# Port Foundry
-opencachefile "../H3MythicMaps/warehouse.map"
-porttag !ms30 *.scnr
-exit
-opencachefile "../H3MCCMaps/s3d_waterfall.map"
-# Port Waterfall
-porttag !ms30 *.scnr
-exit
-edittag levels\multi\guardian\guardian.scenario
-copyforgepalette levels\multi\construct\construct.scenario
-copyforgepalette levels\multi\isolation\isolation.scenario
-copyforgepalette levels\multi\salvation\salvation.scenario
-copyforgepalette levels\multi\snowbound\snowbound.scenario
-copyforgepalette levels\dlc\armory\armory.scenario
-copyforgepalette levels\dlc\chillout\chillout.scenario
-copyforgepalette levels\dlc\descent\descent.scenario
-copyforgepalette levels\dlc\docks\docks.scenario
-copyforgepalette levels\dlc\fortress\fortress.scenario
-copyforgepalette levels\dlc\ghosttown\ghosttown.scenario
-copyforgepalette levels\dlc\lockout\lockout.scenario
-copyforgepalette levels\dlc\midship\midship.scenario
-copyforgepalette levels\dlc\sandbox\sandbox.scenario
-copyforgepalette levels\dlc\sidewinder\sidewinder.scenario
-copyforgepalette levels\dlc\spacecamp\spacecamp.scenario
-copyforgepalette levels\dlc\warehouse\warehouse.scenario
-copyforgepalette levels\multi\s3d_waterfall\s3d_waterfall.scenario
-exit
-
-updatemapfilesalt "../H3MCCMaps/info"
-updatemapfilesalt "../H3MythicMaps/info"
-updatemapfilesalt "../H3Maps/info"
-
 nametag 0x5732 ui\images\placeholder
 nametag 0x5733 ui\images\riverworld
 nametag 0x5734 ui\images\s3d_turf
@@ -184,6 +92,11 @@ edittag ui\images\spacecamp.bitmap
 importbitmap 0 "../DDS/spacecamp.dds"
 savetagchanges
 exit
+createtag bitm ui\images\village
+edittag ui\images\village.bitmap
+importbitmap 0 "../DDS/village.dds"
+savetagchanges
+exit
 createtag bitm ui\images\warehouse
 edittag ui\images\warehouse.bitmap
 importbitmap 0 "../DDS/warehouse.dds"
@@ -191,7 +104,7 @@ savetagchanges
 exit
 
 edittag 0x5731.gfxt
-addblockelements textures 17
+addblockelements textures 18
 setfield textures[13].filename 580
 setfield textures[13].bitmap ui\images\armory.bitmap
 setfield textures[14].filename 600
@@ -224,9 +137,103 @@ setfield textures[27].filename 360
 setfield textures[27].bitmap ui\images\snowbound.bitmap
 setfield textures[28].filename 500
 setfield textures[28].bitmap ui\images\spacecamp.bitmap
-setfield textures[29].filename 480
-setfield textures[29].bitmap ui\images\warehouse.bitmap
+setfield textures[29].filename 
+setfield textures[29].bitmap ui\images\village.bitmap
+setfield textures[30].filename 480
+setfield textures[30].bitmap ui\images\warehouse.bitmap
 savetagchanges
 exit
+
+# Port Construct
+opencachefile "../H3Maps/construct.map"
+porttag !ms30 *.scnr
+exit
+# Port Isolation
+opencachefile "../H3Maps/isolation.map"
+porttag !ms30 *.scnr
+exit
+# Port Epitaph
+opencachefile "../H3Maps/salvation.map"
+porttag !ms30 *.scnr
+exit
+# Port Snowbound
+opencachefile "../H3Maps/snowbound.map"
+porttag !ms30 *.scnr
+exit
+# Port Rat's Nest
+opencachefile "../H3MythicMaps/armory.map"
+porttag !ms30 *.scnr
+exit
+# Port Cold Storage
+opencachefile "../H3MythicMaps/chillout.map"
+porttag !ms30 *.scnr
+exit
+# Port Assembly
+opencachefile "../H3MythicMaps/descent.map"
+porttag !ms30 *.scnr
+exit
+# Port Longshore
+opencachefile "../H3MythicMaps/docks.map"
+porttag !ms30 *.scnr
+exit
+# Port Citadel
+opencachefile "../H3MythicMaps/fortress.map"
+porttag !ms30 *.scnr
+exit
+# Port Ghost Town
+opencachefile "../H3MythicMaps/ghosttown.map"
+porttag !ms30 *.scnr
+exit
+# Port Blackout
+opencachefile "../H3MythicMaps/lockout.map"
+porttag !ms30 *.scnr
+exit
+# Port Heretic
+opencachefile "../H3MythicMaps/midship.map"
+porttag !ms30 *.scnr
+exit
+# Port Sandbox
+opencachefile "../H3MythicMaps/sandbox.map"
+porttag !ms30 *.scnr
+exit
+# Port Avalanche
+opencachefile "../H3MythicMaps/sidewinder.map"
+porttag !ms30 *.scnr
+exit
+# Port Orbital
+opencachefile "../H3MythicMaps/spacecamp.map"
+porttag !ms30 *.scnr
+exit
+# Port Foundry
+opencachefile "../H3MythicMaps/warehouse.map"
+porttag !ms30 *.scnr
+exit
+opencachefile "../H3MCCMaps/s3d_waterfall.map"
+# Port Waterfall
+porttag !ms30 *.scnr
+exit
+edittag levels\multi\guardian\guardian.scenario
+copyforgepalette levels\multi\construct\construct.scenario
+copyforgepalette levels\multi\isolation\isolation.scenario
+copyforgepalette levels\multi\salvation\salvation.scenario
+copyforgepalette levels\multi\snowbound\snowbound.scenario
+copyforgepalette levels\dlc\armory\armory.scenario
+copyforgepalette levels\dlc\chillout\chillout.scenario
+copyforgepalette levels\dlc\descent\descent.scenario
+copyforgepalette levels\dlc\docks\docks.scenario
+copyforgepalette levels\dlc\fortress\fortress.scenario
+copyforgepalette levels\dlc\ghosttown\ghosttown.scenario
+copyforgepalette levels\dlc\lockout\lockout.scenario
+copyforgepalette levels\dlc\midship\midship.scenario
+copyforgepalette levels\dlc\sandbox\sandbox.scenario
+copyforgepalette levels\dlc\sidewinder\sidewinder.scenario
+copyforgepalette levels\dlc\spacecamp\spacecamp.scenario
+copyforgepalette levels\dlc\warehouse\warehouse.scenario
+copyforgepalette levels\multi\s3d_waterfall\s3d_waterfall.scenario
+exit
+
+updatemapfilesalt "../H3MCCMaps/info"
+updatemapfilesalt "../H3MythicMaps/info"
+updatemapfilesalt "../H3Maps/info"
 
 dumplog ED_Enhanced_log.txt
